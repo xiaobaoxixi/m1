e498fdc54920581bbda4925a6900aa26f949fe34
liyan <yanx0167@stud.kea.dk>
2018-09-13 18:29:39 +0200

    added: sorting, including re-align after different sorting

--- display.js
+++ display.js
@@ -12,28 +12,46 @@ function displayList(arr) {
   });
 }
 
+document
+  .querySelector(".function-first-name")
+  .addEventListener("click", byFirstNameClicked);
+function byFirstNameClicked() {
+  sortByFirstName();
+  lineUpLeft();
+}
+
+function lineUpLeft() {
+  document.querySelector(".list").style.textAlign = "left";
+  //first letter change
+}
+
 // click on sort by last name
 document
   .querySelector(".function-last-name")
-  .addEventListener("click", lineUpLastName);
-// line up last names
-function lineUpLastName() {
-  let allLastNames = document.querySelectorAll(
-    ".each-name span:nth-of-type(3)"
-  );
-  let allOtherNames = document.querySelectorAll(
-    ".each-name span:not(:nth-of-type(3))"
-  );
-  document.querySelector(".list").style.textAlign = "right";
-  allLastNames.forEach(item => {
-    item.style.left =
-      item.getBoundingClientRect().width - window.innerWidth * 0.25 + "px";
-  });
-  allOtherNames.forEach(item => {
-    item.style.left =
-      item.parentElement.lastChild.getBoundingClientRect().width -
-      11 -
-      window.innerWidth * 0.25 +
-      "px"; //  add a 11px gap between last name and the names before to make the line up more apparent
-  });
+  .addEventListener("click", byLastNameClicked);
+function byLastNameClicked() {
+  sortByLastName();
+  lineUpLastName();
+
+  // line up last names
+  function lineUpLastName() {
+    let allLastNames = document.querySelectorAll(
+      ".each-name span:nth-of-type(3)"
+    );
+    let allOtherNames = document.querySelectorAll(
+      ".each-name span:not(:nth-of-type(3))"
+    );
+    document.querySelector(".list").style.textAlign = "right";
+    allLastNames.forEach(item => {
+      item.style.left =
+        item.getBoundingClientRect().width - window.innerWidth * 0.25 + "px";
+    });
+    allOtherNames.forEach(item => {
+      item.style.left =
+        item.parentElement.lastChild.getBoundingClientRect().width -
+        11 -
+        window.innerWidth * 0.25 +
+        "px"; //  add a 11px gap between last name and the names before to make the line up more apparent
+    });
+  }
 }
--- main.js
+++ main.js
@@ -25,11 +25,11 @@ function getData() {
   fetch("stud-list.json")
     .then(data => data.json())
     .then(names => {
-      names.forEach(n => createStudObj(n));
+      names.forEach(n => buildArray(n));
       currentList = originalList;
       displayList(currentList);
     });
-  function createStudObj(eachStud) {
+  function buildArray(eachStud) {
     let studObj = Object.create(prototypeStudent);
     studObj.splitName(eachStud);
     originalList.push(studObj);
@@ -38,3 +38,30 @@ function getData() {
 }
 
 window.addEventListener("DOMContentLoaded", init);
+
+function sortByFirstName() {
+  currentList.sort();
+  displayList(currentList);
+}
+
+function sortByLastName() {
+  currentList.sort(sortArrayLastName);
+  function sortArrayLastName(a, b) {
+    if (a.lastName > b.lastName) {
+      return 1;
+    } else if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+  displayList(currentList);
+}
+
+function shuffle(arr) {
+  let indexS = [];
+  for (let i = 0; i < arr.length; i++) {
+    indexS.push(i);
+  }
+  console.log(indexS);
+}
